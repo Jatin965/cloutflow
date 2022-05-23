6e46cce6e741b63c95e92174e4180efcd491e897
Jatin Saini <jatin4762@gmail.com>
2022-05-23 16:28:32 +0530

    task 1 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [arr, setArr] = useState([]);
+  const [text, setText] = useState("");
+
+  const changeHandler = (e) => {
+    setText(e.target.value);
+    if (e.target.value.includes(" ") || e.target.value.includes(",")) {
+      setArr([...arr, e.target.value.replace(/[, ]+/g, "")]);
+      setText("");
+    }
+  };
+
+  console.log(arr);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div className="chips">
+          {arr.map((na, index) => (
+            <span key={index} className="chip">
+              {na}
+            </span>
+          ))}
+        </div>
+        <input
+          className="textarea"
+          value={text}
+          onChange={changeHandler}
+          rows={20}
+          cols={90}
+        />
+      </div>
     </div>
   );
 }
